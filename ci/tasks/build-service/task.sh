b9f6125b84da1c21dd30ed44c5dbad76c68e3698
bkamysz-pivotal <bkamysz@pivotal.io>
2017-08-10 16:26:41 -0700

    Add CI Pipeline

---
+++ ci/tasks/build-service/task.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+pushd workspace
+    ./mvnw clean install
+popd
+
+mkdir build-output/libs
+
+cp fleet-location-service/target/fleet-location-service-1.0.0.BUILD-SNAPSHOT.jar build-output/
+cp fleet-location-simulator/target/fleet-location-simulator-1.0.0.BUILD-SNAPSHOT.jar build-output/
+cp fleet-location-ingest/target/fleet-location-ingest-1.0.0.BUILD-SNAPSHOT.jar build-output/
+cp fleet-location-updater/target/fleet-location-updater-1.0.0.BUILD-SNAPSHOT.jar build-output/
+cp service-location-service/target/service-location-service-1.0.0.BUILD-SNAPSHOT.jar build-output/
+cp dashboard/target/dashboard-1.0.0.BUILD-SNAPSHOT.jar build-output/
+
+cp manifest.yml build-output/
