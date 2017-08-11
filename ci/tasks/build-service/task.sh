7647bc23d1e399455cec7ef0c5082e13fd1876ab
bkamysz-pivotal <bkamysz@pivotal.io>
2017-08-11 14:41:11 -0700

    Remove directory creation

--- ci/tasks/build-service/task.sh
+++ ci/tasks/build-service/task.sh
@@ -5,8 +5,6 @@ pushd vehicle-fleet-demo
     ./mvnw clean install
 popd
 
-mkdir build-output
-
 cp vehicle-fleet-demo/fleet-location-service/target/fleet-location-service-1.0.0.BUILD-SNAPSHOT.jar build-output/
 cp vehicle-fleet-demo/fleet-location-simulator/target/fleet-location-simulator-1.0.0.BUILD-SNAPSHOT.jar build-output/
 cp vehicle-fleet-demo/fleet-location-ingest/target/fleet-location-ingest-1.0.0.BUILD-SNAPSHOT.jar build-output/
