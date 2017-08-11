c28c65fadffddfb21ffc8880cde917fb29985022
bkamysz-pivotal <bkamysz@pivotal.io>
2017-08-10 17:01:36 -0700

    Fix source locations

--- ci/tasks/build-service/task.sh
+++ ci/tasks/build-service/task.sh
@@ -7,11 +7,11 @@ popd
 
 mkdir build-output/libs
 
-cp fleet-location-service/target/fleet-location-service-1.0.0.BUILD-SNAPSHOT.jar build-output/
-cp fleet-location-simulator/target/fleet-location-simulator-1.0.0.BUILD-SNAPSHOT.jar build-output/
-cp fleet-location-ingest/target/fleet-location-ingest-1.0.0.BUILD-SNAPSHOT.jar build-output/
-cp fleet-location-updater/target/fleet-location-updater-1.0.0.BUILD-SNAPSHOT.jar build-output/
-cp service-location-service/target/service-location-service-1.0.0.BUILD-SNAPSHOT.jar build-output/
-cp dashboard/target/dashboard-1.0.0.BUILD-SNAPSHOT.jar build-output/
+cp vehicle-fleet-demo/fleet-location-service/target/fleet-location-service-1.0.0.BUILD-SNAPSHOT.jar build-output/
+cp vehicle-fleet-demo/fleet-location-simulator/target/fleet-location-simulator-1.0.0.BUILD-SNAPSHOT.jar build-output/
+cp vehicle-fleet-demo/fleet-location-ingest/target/fleet-location-ingest-1.0.0.BUILD-SNAPSHOT.jar build-output/
+cp vehicle-fleet-demo/fleet-location-updater/target/fleet-location-updater-1.0.0.BUILD-SNAPSHOT.jar build-output/
+cp vehicle-fleet-demo/service-location-service/target/service-location-service-1.0.0.BUILD-SNAPSHOT.jar build-output/
+cp vehicle-fleet-demo/dashboard/target/dashboard-1.0.0.BUILD-SNAPSHOT.jar build-output/
 
 cp manifest.yml build-output/
